96f22bcbab82d421e0ed9f6ecf7ab7140900fded
vtminh <vutuanminh555@gmail.com>
2025-03-25 21:12:47 +0700

    parameterized

---
+++ param_def.v
@@ -0,0 +1,15 @@
+// constant
+`define MAX_CODE_RATE           3
+`define MAX_CONSTRAINT_LENGTH   9
+`define MAX_STATE_NUM           256 // = 2 ^ (MAX_CONSTRAINT_LENGTH - 1), must be hardcoded         
+`define RADIX                   4 // transition per state
+
+// derivative
+`define MAX_SHIFT_REG_NUM       `MAX_CONSTRAINT_LENGTH-1 
+`define DECODE_BIT_NUM          `RADIX/2
+`define TRACEBACK_DEPTH         5*`MAX_SHIFT_REG_NUM*`MAX_CODE_RATE/2 // equal 5*(K-1)*code rate with radix-2 and half of that with radix-4
+`define DATA_FRAME              `TRACEBACK_DEPTH/`MAX_CODE_RATE
+`define MAX_TRANSITION_NUM      `RADIX*`MAX_STATE_NUM       
+
+
+
--- viterbi_decoder.v
+++ viterbi_decoder.v
@@ -1,31 +1,32 @@
+`include "param_def.v"
 `timescale 1ns / 1ps
 
 module viterbi_decoder(sys_clk,rst,en,i_data,o_data,o_done); // need to change wire width 
 
 input sys_clk,rst,en;
-input [59:0] i_data;
+input [`TRACEBACK_DEPTH - 1:0] i_data;
 
-output [19:0] o_data;
+output [`DATA_FRAME - 1:0] o_data;
 output o_done;
 
 wire en_c,en_e,en_b,en_a,en_m,en_t;
 
-wire [1:0] rx;
+wire [`RADIX - 1:0] rx;
 
-wire [1:0] bck_prv_st [255:0];
+wire [`MAX_CONSTRAINT_LENGTH - 1:0] bck_prv_st [`MAX_STATE_NUM - 1:0];
 
 // encoder
-wire [7:0] gen_poly;
+wire [`MAX_SHIFT_REG_NUM - 1:0] gen_poly;
 wire i_bit;
 wire mode_sel;
 
 wire [31:0] mux;
 
-wire [2:0] dist [1023:0];
+wire [2:0] dist [`MAX_TRANSITION_NUM - 1:0];
 
-wire [7:0] prv_st [255:0];
+wire [`MAX_SHIFT_REG_NUM - 1:0] prv_st [`MAX_STATE_NUM - 1:0];
 
-wire [7:0] sel_node;
+wire [`MAX_SHIFT_REG_NUM - 1:0] sel_node;
 
 control C1 (.clk(sys_clk),
             .rst(rst),
@@ -45,18 +46,18 @@ convolutional_encoder Ce1(.clk(sys_clk),
                         .mode_sel(mode_sel),
                         .o_mux(mux));
 
-extract_bit Ex1 (   .rst(rst),
-                    .clk(sys_clk),
-                    .en_e(en_e),
-                    .i_data(i_data),
-                    .o_Rx(rx));
-
 branch_metric Br1 (.clk(sys_clk),
                     .rst(rst),
                     .en_b(en_b),
                     .i_Rx(rx),
                     .i_mux(mux),
                     .o_dist(dist));
+
+extract_bit Ex1 (   .rst(rst),
+                    .clk(sys_clk),
+                    .en_e(en_e),
+                    .i_data(i_data),
+                    .o_Rx(rx));
 
 add_compare_select Add1 (   .clk(sys_clk),
                             .rst(rst),
